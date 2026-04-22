97fa01ee06c9e48eacb8dcafc5f798e6b7ecf633
huangdihd <hd20100104@163.com>
2026-04-22 19:13:55 +0800

    Refactor Envy plugin to use PublicChatEvent and update plugin.yml

--- src/main/java/huangdihd/xinbot/Envy.java
+++ src/main/java/huangdihd/xinbot/Envy.java
@@ -1,24 +1,20 @@
 package huangdihd.xinbot;
 
 import xin.bbtt.mcbot.Bot;
-import xin.bbtt.mcbot.Utils;
 import xin.bbtt.mcbot.event.EventHandler;
 import xin.bbtt.mcbot.event.Listener;
-import xin.bbtt.mcbot.events.SystemChatMessageEvent;
+import xin.bbtt.mcbot.events.PublicChatEvent;
 import xin.bbtt.mcbot.plugin.Plugin;
-
-import java.util.ArrayList;
 
 class listener implements Listener {
     @EventHandler
-    public void onChatMessage(SystemChatMessageEvent event) {
-        ArrayList<String> strings = Utils.toStrings(event.getContent());
-        if (strings.size() != 3) return;
-        if (!strings.get(1).contains("<") || !strings.get(1).contains(">")) return;
-        if (!strings.get(2).contains("入")) return;
-        String result = strings.get(2).replaceFirst("^.*?入", "");
+    public void onChatMessage(PublicChatEvent event) {
+        if (Bot.INSTANCE.getConfig().getConfigData().getAccount().getName().equals(event.getSender().getName())) return;
+        String message = event.getMessage();
+        if (!message.contains("入")) return;
+        String result = message.replaceFirst("^.*?入", "");
         if (result.isBlank()) return;
-        Bot.Instance.sendChatMessage("羡慕" + result);
+        Bot.INSTANCE.sendChatMessage("羡慕" + result);
     }
 }
 
@@ -33,7 +29,7 @@ public class Envy implements Plugin {
 
     @Override
     public void onEnable() {
-        Bot.Instance.getPluginManager().registerEvents(new listener(), this);
+        Bot.INSTANCE.getPluginManager().registerEvents(new listener(), this);
     }
 
     @Override
